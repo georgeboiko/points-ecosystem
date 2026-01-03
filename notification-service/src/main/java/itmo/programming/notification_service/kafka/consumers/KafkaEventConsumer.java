0bd712bbfaaa320da6cb624faadaea79c1ba5f33
George <georgeboiko0411@gmail.com>
2026-01-03 18:40:03 +0300

    [fix]: kafka fix

--- notification-service/src/main/java/itmo/programming/notification_service/kafka/consumers/KafkaEventConsumer.java
+++ notification-service/src/main/java/itmo/programming/notification_service/kafka/consumers/KafkaEventConsumer.java
@@ -11,12 +11,22 @@ public class KafkaEventConsumer {
     private static final String POINTS_ADDED_TOPIC = "points-added";
     private static final String POINTS_DELETED_TOPIC = "points-deleted";
 
-    @KafkaListener(topics = POINTS_ADDED_TOPIC)
+    @KafkaListener(
+            topics = POINTS_ADDED_TOPIC,
+            properties = {
+                    "spring.json.value.default.type=itmo.programming.notification_service.kafka.events.PointsAddedEvent"
+            }
+    )
     public void consumeAddedPoints(PointsAddedEvent pointsAddedEvent) {
         System.out.println(pointsAddedEvent.toString());
     }
 
-    @KafkaListener(topics = POINTS_DELETED_TOPIC)
+    @KafkaListener(
+            topics = POINTS_DELETED_TOPIC,
+            properties = {
+                    "spring.json.value.default.type=itmo.programming.notification_service.kafka.events.PointsDeletedEvent"
+            }
+    )
     public void consumeDeletedPoints(PointsDeletedEvent pointsDeletedEvent) {
         System.out.println(pointsDeletedEvent.toString());
     }
